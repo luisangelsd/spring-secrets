84c04f6d7ac9acf5ca20a490a121d443324234bc
Luis Angel Sandoval Medina <sandovalguichoo@gmail.com>
2023-05-01 14:41:29 -0600

    entity-rol-y-usuario-ok

---
+++ src/main/java/com/secrets/dao/oauth2/entitys/EntityRol.java
@@ -0,0 +1,48 @@
+package com.secrets.dao.oauth2.entitys;
+
+import java.io.Serializable;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "roles")
+public class EntityRol implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true, length = 20)
+	private String nombre;
+
+
+	
+	//-- Getters and Setters
+	public Integer getId() {return id;	}
+	public void setId(Integer id) {	this.id = id;	}
+	public String getNombre() {	return nombre;	}
+	public void setNombre(String nombre) {this.nombre = nombre;	}
+	
+	
+	//-- Constructores
+	public EntityRol(Integer id, String nombre) {
+		this.id = id;
+		this.nombre = nombre;
+	}
+	
+	public EntityRol() {
+
+	}
+	
+	
+}
---
+++ src/main/java/com/secrets/dao/oauth2/entitys/EntityUsuario.java
@@ -0,0 +1,105 @@
+package com.secrets.dao.oauth2.entitys;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.FetchType;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.PrePersist;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import javax.validation.constraints.NotEmpty;
+
+
+
+@Entity
+@Table(name = "usuarios")
+public class EntityUsuario implements Serializable{
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@NotEmpty(message = "El username no puede estar vacio")
+	@Column(unique = true)
+	private String username;
+	
+	@NotEmpty(message = "El Password no puede estar vacia")
+	@Column(length = 60)
+	private String password;
+	
+	private Boolean enabled;
+	
+	@Column(name = "descripcion_perfil")
+	
+	private String descripcionPerfil;
+	
+	@ManyToMany(fetch = FetchType.LAZY, cascade = CascadeType.ALL)
+	@JoinTable(name = "usuario_roles",
+	joinColumns = @JoinColumn(name="usuario_id"),
+	inverseJoinColumns = @JoinColumn(name="rol_id"),
+	uniqueConstraints = {@UniqueConstraint(columnNames = {"usuario_id","rol_id"})})
+	private List<EntityRol> roles;
+	
+	
+	//-- Valores por defecto
+	@PrePersist
+	private void datosPorDefecto() {
+		this.enabled=true;
+		this.descripcionPerfil="Sin descripción aún";
+	}
+
+	
+	//-- Getters and setters
+	public Integer getId() {return id;	}
+	public void setId(Integer id) {	this.id = id;}
+	public String getUsername() {	return username;}
+	public void setUsername(String username) {	this.username = username;}
+	public String getPassword() {	return password;}
+	public void setPassword(String password) {	this.password = password;}
+	public Boolean getEnabled() {	return enabled;	}
+	public void setEnabled(Boolean enabled) {	this.enabled = enabled;	}
+	public String getDescripcionPerfil() {return descripcionPerfil;	}
+	public void setDescripcionPerfil(String descripcionPerfil) {this.descripcionPerfil = descripcionPerfil;	}
+	public List<EntityRol> getRoles() {	return roles;}
+	public void setRoles(List<EntityRol> roles) {	this.roles = roles;	}
+
+	
+	//-- Constructores
+	public EntityUsuario(Integer id, @NotEmpty(message = "El username no puede estar vacio") String username,
+			@NotEmpty(message = "El Password no puede estar vacia") String password, Boolean enabled,
+			String descripcionPerfil, List<EntityRol> roles) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.password = password;
+		this.enabled = enabled;
+		this.descripcionPerfil = descripcionPerfil;
+		this.roles = roles;
+	}
+
+	public EntityUsuario() {
+	}
+	
+	
+	
+
+	
+	
+	
+
+	
+}
