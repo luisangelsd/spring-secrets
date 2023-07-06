07b06a07ce1d19715a5817fb452ce776922d86a5
Sandoval Güicho <sandovalguichoo@gmail.com>
2023-07-06 09:57:45 -0600

    controllerCategorias

---
+++ src/test/java/com/secrets/dao/controladores/ControladorCategoriasTest.java
@@ -0,0 +1,51 @@
+package com.secrets.dao.controladores;
+
+import com.secrets.dao.Datos;
+import com.secrets.dao.modelo.servicios.IServicesCategorias;
+import com.secrets.dao.modelo.servicios.IServicesUsuarios;
+import org.junit.jupiter.api.*;
+import org.mockito.Mockito;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@SpringBootTest
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+@AutoConfigureMockMvc
+class ControladorCategoriasTest {
+
+    //-- Inyecciónes
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private IServicesCategorias iServicesCategorias;
+
+
+    //------------------------------------------------------------------------------------------------
+    @Test
+    @Order(1)
+    @DisplayName("listarCategorias() - Regresa Valores")
+    void listarCategorias() throws Exception {
+
+        //--Datos
+        Mockito.when(this.iServicesCategorias.listarCategorias()).thenReturn(Datos.getListarCategorias());
+
+        //-- Servvicio + Test
+        this.mockMvc.perform(MockMvcRequestBuilders.get("/categorias/listar/")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_JSON));
+
+    }
+
+
+}
